472359fdc9e26ab1a0545901ed1d36848d431644
SumanSaha <8759127658itisaha@gmail.com>
2024-05-06 21:51:31 +0530

    Add bcrypt for password hashing in user registration; update dependencies and devDependencies
    This commit adds bcrypt for password hashing in user registration in the `registerUser.js` file. It also updates several dependencies and devDependencies

--- server/controllers/registerUser.js
+++ server/controllers/registerUser.js
@@ -1,28 +1,33 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt')
 const User = require('../models/user.model');
 const generateToken = require('../config/generateToken')
 
 const registerUser = asyncHandler(async(req,res)=>{
     //get data from the req.body
-    const {name,email,password,confirmPassword,pic} = req.body;
+    const {name,email,password,pic} = req.body;
 
-    if(!name||!email||!password||!confirmPassword){
+    // checking if all fields are filled
+    if(!name|| !email|| !password){
         res.status(400)
         throw new Error("Please provide all fields");
     }
 
+    // checking if user already exists via email
     const userExists = await User.findOne({email});
 
+    // if user exists then throw error
     if(userExists){
         res.status(400);
         throw new Error ("User already exists")
     }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
     const user = await User.create({
         name,
         email,
-        password,
-        confirmPassword,
+        password:hashedPassword,
         pic
     })
 
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,14 +1,38 @@
 const mongoose = require('mongoose');
 
 const User = mongoose.Schema({
-    name:{type: String,required:true},
-    email:{type: String,required:true,unique:true},
-    password:{type: String, required: true,unique:true},
-    confirmPassword:{ type : String ,matches:"password",required: true },   //validating the field against another field
-    profilePic:{type:String,default:"https://i.pinimg.com/originals/09/21/fc/0921fc87aa989330b8d403014bf4f340.jpg"}
-},
-{timestamps:true})
+    name: {
+        type: String,
+        required: true
+    },
 
-const user = mongoose.model("User", User); 
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    profilePic: {
+        type: String,
+        default: "https://i.pinimg.com/originals/09/21/fc/0921fc87aa989330b8d403014bf4f340.jpg"
+    }
+},
+    {
+        timestamps: {
+            createdAt: 'created_at', // Rename createdAt to created_at
+            updatedAt: 'updated_at', // Rename updatedAt to updated_at
+            format: 'YYYY-MM-DD HH:mm:ss' // Format timestamps
+        }
+    })
+
+const user = mongoose.model("User", User);
 
 module.exports = user
+
+
+
